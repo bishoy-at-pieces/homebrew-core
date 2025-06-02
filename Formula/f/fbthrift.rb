dbf469c10403bfa77b0d61fe1ab9d09d48dd131d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-02 17:03:34 +0000

    fbthrift 2025.06.02.00

--- Formula/f/fbthrift.rb
+++ Formula/f/fbthrift.rb
@@ -1,8 +1,8 @@
 class Fbthrift < Formula
   desc "Facebook's branch of Apache Thrift, including a new C++ server"
   homepage "https://github.com/facebook/fbthrift"
-  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2025.05.26.00.tar.gz"
-  sha256 "28b0ba5d5698369c17919d68656ee20e0a85c789f76a08b3e718ae97a7ddc235"
+  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2025.06.02.00.tar.gz"
+  sha256 "f2a66c299f341cede6fc06acb4e795ac0ee50ff00bb3b33e6bab6a2cfc3efc79"
   license "Apache-2.0"
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
